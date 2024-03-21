1be7847672977b56a15cb51f53983e8051213553
GyunNote <rlaehrbs1968@naver.com>
2024-03-21 17:17:21 +0900

    google log-in fin

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignupPage from "../SignupPage/SignupPage";
 import SigninPage from "../SigninPage/SigninPage";
+import OAuth2Page from "../OAuth2Page/OAuth2Page";
+import OAuth2SignupPage from "../OAuth2SignupPage/OAuth2SignupPage";
+import OAuth2SigninPage from "../OAuth2SigninPage/OAuth2SigninPage";
 
 function AuthPage(props) {
     return (
@@ -11,7 +14,10 @@ function AuthPage(props) {
             <Routes>
                 <Route path='/signin' element={<SigninPage/>}/>
                 <Route path='/signup' element={<SignupPage/>}/>
-                <Route path='/signup/oauth'/>
+                <Route path='/oauth2' element={<OAuth2Page/>}/>
+                <Route path='/oauth2/signin'element={<OAuth2SigninPage/>} />
+                <Route path='/oauth2/merge' />
+                <Route path='/oauth2/signup' element={<OAuth2SignupPage/>}/>
             </Routes>
         </div>
     );
